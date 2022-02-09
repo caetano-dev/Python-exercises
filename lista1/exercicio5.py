1299dd59cb5fac4a420555ef969f6159d628bf06
drull1000 <pcaetanop@protonmail.com>
2022-02-09 18:00:20 -0300

    5th exercise

---
+++ lista1/exercicio5.py
@@ -0,0 +1,19 @@
+'''
+5. Crie um algoritmo que imprima o peso total que será carregado por um caminhão.
+Sabe-se que este caminhão carrega 25 caixas. O peso de cada caixa deve ser informado
+pelo usuário.
+'''
+
+pesoDiferente = input("digite D caso as caixas tenham peso diferente.")
+pesoTotal = 0
+
+if pesoDiferente == "D":
+    for i in range(25):
+        peso = float(input("digite o peso."))
+        pesoTotal += float(peso)
+    print(pesoTotal)
+else:
+    peso = float(input("digite o peso."))
+    pesoTotal = 25 * float(peso)
+    print(pesoTotal)
+
