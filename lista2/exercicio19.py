661275a118fa9ad00f9a737176cb9a67af5f6370
drull1000 <pcaetanop@protonmail.com>
2022-05-16 08:31:00 -0300

    backup

--- lista2/exercicio19.py
+++ lista2/exercicio19.py
@@ -1,9 +1,8 @@
 #Crie um algoritmo que leia um número N e imprima os N primeiros números
 # primos. O seu programa deve fazer o MÍNIMO de interações possíveis.
-number = int(input("number: "))
-primo = True
-for c in range(2,number):
-    if number % c == 0:
-        primo = False
-        break
-print(primo)
+def isprime(num):
+    for n in range(2,int(num**0.5)+1):
+        if num%n==0:
+            return False
+    return True
+print(isprime(7))
